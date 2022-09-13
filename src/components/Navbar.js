2e999ef11234fd0ec3dd54df8eca1e79c21394a3
2deadmen <kartikhegde.2002@gmail.com>
2022-09-13 21:05:30 +0530

    sticky Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar =()=>{
   
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
           <Link className="navbar-brand" to="/">
             <h1>
               {" "}
